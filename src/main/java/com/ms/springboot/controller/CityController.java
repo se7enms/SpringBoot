185d698395a8920fa6652527cf73dd05972ed466
se7enms <se7enmmss@gmail.com>
2018-10-24 22:29:44 +0800

    修改前端提交数据页面

--- src/main/java/com/ms/springboot/controller/CityController.java
+++ src/main/java/com/ms/springboot/controller/CityController.java
@@ -58,10 +58,10 @@ public class CityController {
             model.addAttribute("action", "addCity");
         } else if (updateValue.equals(state)) {
             //获取编辑城市的信息
-            String ID = request.getParameter("ID");
-            map.put("ID",ID);
+            String cityID = request.getParameter("ID");
+            map.put("ID",cityID);
             model.addAttribute("cityInfo", cityService.findCityByName(map));
-            model.addAttribute("ID", ID);
+            model.addAttribute("ID", cityID);
             model.addAttribute("action", "updateCity");
         }
         return CITY_FORM_PATH_NAME;
